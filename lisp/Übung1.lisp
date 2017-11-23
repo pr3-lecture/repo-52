1468bb2412d67e90daa2e90839aacf7a6d2dbcbc
Philipp Minges <fjnalta@gmail.com>
2017-11-23 15:36:37 +0100

    LISP - add binary-tree

--- lisp/Übung1.lisp
+++ lisp/Übung1.lisp
@@ -32,14 +32,15 @@
 ; (d) Länge einer geschachtelten Liste berechnen: Schreiben Sie eine Funktion 
 ; my-lengthR zur Berechnung der Länge einer Liste und aller eingeschachtelten
 ; Listen.
+
+; TODO - does not work
 (defun my-lengthR (list)
     (if list
-        (if (listp list)
-            (1+ (my-lengthR (cdr list)))
-        0)
         (1+ (my-lengthR (cdr list)))
     0)
-) ; TODO Listen mit mehreren Ebenen
+) 
+
+
 
 ; (e) Listen umkehren: Schreiben eine Funktion my-reverse zum Umkehren 
 ; einer Liste.
@@ -56,12 +57,85 @@
 ; (f) Geschachtelte Listen umkehren: Schreiben eine Funktion my-reverseR 
 ; zum Umkehren einer Liste.
 
+; TODO
+
 ; Aufgabe 2
 ;----------
 
 ; (a) Darstellung eines Binärbaums: Überlegen Sie, wie Sie mittels einer Liste 
 ; einen Binärbaum darstellen können.
 
+; Ein Binärbaum besteht aus einer Menge von Knoten.
+; Jeder Knoten eines Binärbaums besitzt einen Wert und jeweils zwei Nachfolger.
+; Es gibt einen rechten und linken Nachfolger. Der linke Nachfolger ist immer kleiner, der rechte größer.
+
+; -> ein Binärbaum kann durch geschachtelte Listen dargestellt werden. Jedes Element des Binärbaums besteht aus einer Liste
+; mit 3 Elementen. Dem Wert und der Liste des rechten sowie des linken Nachfolgers.
+
+; Beispiel: (3 5 8 4 2)
+; Baum:      3
+;           / \
+;          2   5
+;             / \
+;            4   8
+
+; [ohne Behebung unausgewogener Bäume]
+
+; Definitionen
+; ------------
+
+; Baumstruktur = Liste aus drei Elementen
+(defun create-tree-structure (root left right)
+    (list root left right)
+)
+
+; Wurzel = erstes Element der Liste
+(defun root (tree)
+    (car tree)
+)
+
+; linker Nachfolger = zweites Element der Liste
+(defun left-follower (tree)
+    (cadr tree)
+)
+
+; rechter Nachfolger = drittes Element der Liste
+(defun right-follower (tree)
+    (caddr tree)
+)
+
+; Operationen
+; -----------
+
+; erstelle Baum aus Liste
+(defun create-tree (list)
+    (if (not (boundp 'tree))
+        (setf tree nil)
+    )
+    (dolist (node list)
+        (setf tree (tree-insert node tree)) ; füge aktuelles Element in Baum ein
+    )
+)
+
+; dolist (var list-form [result-form]) declaration* {tag | statement}*
+; (dolist 
+;   (x '(a b c d)) (print x)
+; )
+; Für jedes Element "x" der Liste, wird print ausgeführt. - Ergebnis:
+; A
+; B
+; C
+; D
+
+
+(defun tree-insert (node tree)
+    (cond 
+        ((null tree) (create-tree-structure node nil nil)) ; wenn der Baum null ist, erzeuge neuen Baum
+        ((= node (root tree)) tree) ; gebe ursprünglichen Baum aus, wenn Element bereits vorhanden
+        ((< node (root tree)) (create-tree-structure (root tree) (tree-insert node (left-follower tree)) (right-follower tree)))
+        ((> node (root tree)) (create-tree-structure (root tree) (left-follower tree) (tree-insert node (right-follower tree))))
+    )
+)
 
 ; (b) Baumtraversierung: Schreiben Sie 3 Funktionen zum Traversieren eines 
 ; Binärbaums, bei der auch die Knoteninhalte ausgegeben werden.
