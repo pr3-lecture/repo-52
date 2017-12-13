a7eb69835a82e5fbc8bfdc12e34b274cb1b64f76
Philipp Minges <fjnalta@gmail.com>
2017-12-13 16:02:33 +0100

    LISP - bugfix remove

--- lisp/Übung2.lisp
+++ lisp/Übung2.lisp
@@ -1,22 +1,115 @@
 ;Dimitry Nagorny, 1410351, repo-50
 ;Philipp Minges, 1426312, repo-52
 
+; Aufgabe 1
+;----------
+
+; (a) Elemente tauschen: Schreiben Sie eine Funktion rotiere, die eine Liste 
+; als Argument erhäalt und eine neue Liste zuruckliefert, in der das 
+; vormals erste Element nun das letzte ist.
+(defun rotiere (arg1)
+    (append(rest arg1) (list (first arg1)))
+)
+(print "Rotiere Liste (eins zwei drei vier):")
+(print (rotiere '(eins zwei drei vier)))
+
+; (b) Element einfugen : Schreiben Sie eine Funktion neues-vorletztes , 
+; die eine Liste als Argument erhält und eine neue Liste zuruckliefert, 
+; in der das vormals erste Element nun das letzte ist.
+(defun abl (list)
+    (loop for l on list
+        while (rest l)
+            collect (first l))
+)
+(defun neues-vorletztes(e l) 
+    (append (append (abl l) (list e) (last l)))
+)
+(print "Neues Vorletztes 'dreieinhalb zur Liste (eins zwei drei vier):")
+(print (neues-vorletztes 'dreieinhalb '(eins zwei drei vier)))
+
+
+; (c) Länge einer Liste berechnen: Schreiben Sie eine Funktion my-length
+; zur Berechnung der Länge einer Liste.
+(defun my-length (list)
+    (cond ((null list) 0)
+        (T
+            (+ 1 (my-length (cdr list)))
+        )
+    )
+)
+(print "Length der Liste (eins zwei drei vier):")
+(print (my-length '(eins zwei drei vier)))
+
+; (d) Länge einer geschachtelten Liste berechnen: Schreiben Sie eine Funktion 
+; my-lengthR zur Berechnung der Länge einer Liste und aller eingeschachtelten
+; Listen.
+(defun my-lengthR (list)
+    (cond ((null list) 0) ; länge 0 bei leerer Liste
+        ((listp (car list)) 
+            (+ (my-lengthR (car list)) (my-lengthR (cdr list))) ; wenn erstes Element eine Liste ist,
+            ; addiere das Ergebnis der inneren Liste mit dem Rest
+        ) 
+        (T
+            (+ 1 (my-lengthR (cdr list))) ; bei Atom addiere 1 und rekursiv mit dem Rest der Liste
+        )
+    )
+)
+(print "Verschachtelte Length der Liste (eins zwei (zwei (zwei drei) eins) drei vier):")
+(print (my-lengthR '(eins zwei (zwei (zwei drei) eins) drei vier)))
+
+; (e) Listen umkehren: Schreiben eine Funktion my-reverse zum Umkehren 
+; einer Liste.
+(defun my-reverse (list)
+    (cond ((null list) '())
+        (T 
+            (append
+                (my-reverse (cdr list))
+                (list (car list))
+            )
+        )
+	)
+)
+(print "Umdrehen der Liste (eins zwei (zwei (zwei drei) eins) drei vier):")
+(print (my-reverse '(eins zwei (zwei (zwei drei) eins) drei vier)))
+
+; (f) Geschachtelte Listen umkehren: Schreiben eine Funktion my-reverseR 
+; zum Umkehren einer Liste.
+(defun my-reverseR (list)
+    (cond ((null list) '()) ; ist list die leere Liste?
+        ((listp (car list)) ; wenn das aktuelle Element eine Liste ist
+            (append 
+                (my-reverseR (cdr list)) 
+                (list (my-reverseR (car list)))
+            )
+        )
+        (T ; sonst wie oben
+            (append 
+                (my-reverseR (cdr list)) 
+                (list (car list))
+            )
+        )
+    )
+)
+(print "Verschachteltes Umdrehen der Liste (eins zwei (zwei (zwei drei) eins) drei vier):")
+(print (my-reverseR '(eins zwei (zwei (zwei drei) eins) drei vier)))
+
+; Aufgabe 2
 ; Baumstruktur = Liste aus drei Elementen
 (defun create-tree-structure (root left right)
     (list root left right)
 )
 
-; Wurzel
+; Wurzel = erstes Element der Liste
 (defun root (tree)
     (car tree)
 )
 
-; linker Nachfolger
+; linker Nachfolger = zweites Element der Liste
 (defun left-follower (tree)
     (cadr tree)
 )
 
-; rechter Nachfolger
+; rechter Nachfolger = drittes Element der Liste
 (defun right-follower (tree)
     (caddr tree)
 )
@@ -37,8 +130,8 @@
 ; füge Knoten in Baum ein
 (defun tree-insertNode (node tree)
     (cond 
-        ((null tree) (create-tree-structure node nil nil))
-        ((= node (root tree)) tree)
+        ((null tree) (create-tree-structure node nil nil)) ; wenn der Baum null ist, erzeuge neuen Baum
+        ((= node (root tree)) tree) ; gebe ursprünglichen Baum aus, wenn Element bereits vorhanden
         ((< node (root tree)) (create-tree-structure (root tree) (tree-insertNode node (left-follower tree)) (right-follower tree)))
         ((> node (root tree)) (create-tree-structure (root tree) (left-follower tree) (tree-insertNode node (right-follower tree))))
     )
@@ -52,7 +145,49 @@
 (setf tree (tree-insertNode 1 tree))
 (print tree)
 
-; füge Werte aus Liste in Baum ein
+; (b) Baumtraversierung: Schreiben Sie 3 Funktionen zum Traversieren eines 
+; Binärbaums, bei der auch die Knoteninhalte ausgegeben werden.
+
+; inorder
+(defun inorder (tree)
+    (cond ((null tree))
+        (T 
+            (inorder (left-follower tree))
+            (print (root tree))
+            (inorder (right-follower tree))
+        )
+    )
+)
+(print "Inorder vom Binärbaum (3 5 8 4 2):")
+(inorder tree)
+
+; postorder
+(defun postorder (tree)
+    (cond ((null tree))
+        (T 
+            (postorder (left-follower tree))
+            (postorder (right-follower tree))
+            (print (root tree))
+        )
+    )
+)
+(print "Postorder vom Binärbaum (3 5 8 4 2):")
+(postorder tree)
+
+; preoder
+(defun preorder (tree)
+    (cond ((null tree))
+        (T 
+            (print (root tree))
+            (preorder (left-follower tree))
+            (preorder (right-follower tree))
+        )
+    )
+)
+(print "Preorder vom Binärbaum (3 5 8 4 2):")
+(preorder tree)
+
+;füge Werte aus Liste in baum ein
 (defun tree-insertListValues (li tri)
 	(if li
 		(let ((newTree (tree-insertNode (first li) tri)))
@@ -62,7 +197,7 @@
 	)
 )
 
-; Werte aus Datei in Liste umwandeln
+;Werte aus Datei in Liste umwandeln
 (defun getListFromFile (filePath)
 	(with-open-file (in filePath)
 		(loop for val = (read in nil)
@@ -78,12 +213,11 @@
 		(tree-insertListValues listValues tri)
 	)
 )
-
 (setf tree (tree-insertFileValues "./nodes.txt" tree))
 (print "Insert von 10 20 30 aus der Datei nodes.txt zum Tree:")
 (print tree)
 
-; Contains
+;Check ob ein Element im Baum vorhanden ist
 (defun tree-containsValue (node tree)
 	(if tree
 		(cond 
@@ -99,9 +233,9 @@
 (print "Ist 2 im Baum?")
 (print (if (tree-containsValue 2 tree) "Ja." "Nein."))
 
-; Size
+;Ermittelt Anzahl aller Nodes im Baum
 (defun tree-size (tree)
-    (cond ((null tree) 0)
+    (cond ((null tree) 0) ; länge 0 bei leerem Baum
         ((listp (root tree)) 
             (+ (tree-size (root tree)) (tree-size (left-follower tree)) (tree-size (right-follower tree))) 
         ) 
@@ -113,18 +247,17 @@
 (print "Anzahl aller Knoten im Baum:")
 (print (tree-size tree))
 
-; getMax
+;Ermittelt grössten Wert im Baum
 (defun tree-maxValue (tree)
     (if (right-follower tree)
 		(tree-maxValue (right-follower tree))
 		(root tree)
 	)
 )
-
 (print "Biggest Wert im Baum:")
 (print (tree-maxValue tree))
 
-; getMin
+;Ermittelt kleinsten Wert im Baum
 (defun tree-minValue (tree)
     (if (left-follower tree)
 		(tree-minValue (left-follower tree))
@@ -134,20 +267,19 @@
 (print "Kleinster Wert im Baum:")
 (print (tree-minValue tree))
 
-; isEmpty
+;Checkt ob der Baum keine Knoten hat
 (defun tree-isEmpty (tree)
     (if (root tree)
 		nil
 		T
 	)
 )
-
 (print "Ist aktueller Baum leer?")
 (print (if (tree-isEmpty tree) "Ja." "Nein."))
 (print "Ist Baum (NIL NIL NIL) leer?")
 (print (if (tree-isEmpty '(NIL NIL NIL)) "Ja." "Nein."))
 
-; height
+;Ermittelt Höhe des Baums
 (defun tree-height (tree)
 	(if (tree-isEmpty tree)
 		0
@@ -168,35 +300,56 @@
 (print "Height des Baumes (4 (3 (2 NIL NIL) NIL) (5 NIL NIL)):")
 (print (tree-height '(4 (3 (2 NIL NIL) NIL) (5 NIL NIL))))
 
-; remove
+;Entfernt Wert ausm Baum, falls vorhanden
 (defun tree-remove (val tree)
-	(cond
-		((null (root tree)) nil)
-    	((and (= val (root tree)) (= (tree-height tree) 1)) nil)
-    	((and (= val (root tree)) (null (right-follower tree)))
-        	(create-tree-structure (tree-maxValue (left-follower tree)) (remove (left-follower tree) (tree-maxValue (left-follower tree))) nil)
-    	)
-    	(
-      		(= val (root tree))
-        	(create-tree-structure (tree-minValue (right-follower tree)) (left-follower tree) (tree-remove (tree-minValue (right-follower tree)) (right-follower tree)))
-    	)
-    	(
-      	(< val (root tree))
-        	(create-tree-structure (root tree) (tree-remove val (left-follower tree)) (right-follower tree))
-    	)
-    	(
-      	(> val (root tree))
-        	(create-tree-structure (root tree) (left-follower tree) (tree-remove val (right-follower tree)))
-    	)
+	(if (tree-containsValue val tree)
+		(cond
+			((null (root tree)) nil)
+			((and (= val (root tree)) (= (tree-height tree) 1)) nil)
+			((and (= val (root tree)) (null (right-follower tree)))
+				(create-tree-structure 
+					(tree-maxValue (left-follower tree)) 
+					(tree-remove (tree-maxValue (left-follower tree)) (left-follower tree))
+					nil
+				)
+			)
+			(
+				(= val (root tree))
+				(create-tree-structure 
+					(tree-minValue (right-follower tree)) 
+					(left-follower tree) 
+					(tree-remove (tree-minValue (right-follower tree)) (right-follower tree))
+				)
+			)
+			(
+			(< val (root tree))
+				(create-tree-structure (root tree) (tree-remove val (left-follower tree)) (right-follower tree))
+			)
+			(
+			(> val (root tree))
+				(create-tree-structure (root tree) (left-follower tree) (tree-remove val (right-follower tree)))
+			)
+		)
+		tree
 	)
 )
-
+;Entferne nicht vorhandenen Wert
 (print "Entferne 7 aus dem Baum:")
 (print (tree-remove 7 tree))
+;Entferne Wert mit rechts nil und links vorhanden
 (print "Entferne 2 aus dem Baum:")
 (print (tree-remove 2 tree))
+;Entferne Wert mit beide teilbäume nil
+(print "Entferne 1 aus dem Baum:")
+(print (tree-remove 1 tree))
+;Entferne Wert mit links nil rechts vorhanden
+(print "Entferne 8 aus dem Baum:")
+(print (tree-remove 8 tree))
+;Entferne Wert der root ist und beide teilbäume vorhanden
+(print "Entferne 3 aus dem Baum:")
+(print (tree-remove 3 tree))
 
-; addAll
+;Fügt aktuellem tri einen gegebenen Baum otherTree hinzu
 (defun tree-addAll (otherTree tri)
 	(if (not (tree-isEmpty otherTree))
 		(let ((newTree (tree-insertNode (root otherTree) tri)))
@@ -224,16 +377,17 @@
         )
     )
 )
-
 ; print levelorder - iterativ :(
 (defun levelorder (tree)
     (setq res nil)
     (loop for i from 1 to (tree-height tree)
-        do (setq res (append res (curLevel i tree))
+        do (
+			setq res (append res (curLevel i tree))
         )
   )
   res
 )
-
 (print "Levelorder vom aktuellen Baum:")
-(levelorder tree)
+(print (levelorder tree))
+    
+(setf tree '())
