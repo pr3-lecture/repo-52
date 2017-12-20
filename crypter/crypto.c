3aa21e73dffb18e1a4de64c12191ba5f80891c48
Philipp Minges <fjnalta@gmail.com>
2017-12-20 19:09:58 +0100

    C - Add Crypter

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -2,29 +2,66 @@
 #include <string.h>
 #include "crypto.h"
 
-int encrypt(KEY key, const char* input, char* output) {
-    printf("INPUT: %s",input);
-    printf("KEY: %s",key.chars);
-    
-    while(*input != '\0') {
-        // dirty ASCII
-        if(*input < 'A' || *input > 'Z') {
-            fprintf(stderr, "Ungültige Nachricht\n");
-            return E_MESSAGE_ILLEGAL_CHAR;
-        }
-        input++;
+int checkIllegalChars(const char* input, int encrypt) {
+    char* chars;
+    if(encrypt == 1) {
+        chars = MESSAGE_CHARACTERS;
+    } else {
+        chars = CYPHER_CHARACTERS;
     }
 
-
-    strcpy(output, "Encrypting output");
+    for(int i = 0; i < strlen(input); i++) {
+        int isCorrectChar = 0;
+        for(int j = 0; j < strlen(chars); j++) {
+            if(input[i] == chars[j]) {
+                isCorrectChar = 1;
+                break;
+            }
+        }
+        if (isCorrectChar == 0) {
+            return 1;
+        }
+    }
     return 0;
 }
 
+int getIndex(char c, const char* strg) {
+    for (int i = 0; i < *strg; i++) {
+        if (strg[i] == c) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int encrypt(KEY key, const char* input, char* output) {
+    if(checkIllegalChars(input, 1) != 0) {
+        fprintf(stderr, "Ungültige Nachricht\n");
+        return E_MESSAGE_ILLEGAL_CHAR;
+    }
+
+    for(int i = 0; i < strlen(input); i++) {
+        int indexInput = getIndex(input[i], MESSAGE_CHARACTERS) + 1;
+        int indexKey = getIndex(key.chars[i % strlen(key.chars)], KEY_CHARACTERS) + 1;
+        output[i] = CYPHER_CHARACTERS[indexInput ^ indexKey];
+    }
+    output[strlen(input)] = '\0';
+    return 0;
+}
+
+
 int decrypt(KEY key, const char* cypherText, char* output) {
-    
-    
-    
-    
-    strcpy(output, "Decrypting output");
+    if(checkIllegalChars(cypherText, 0) != 0) {
+        fprintf(stderr, "Ungültige Nachricht\n");
+        return E_CYPHER_ILLEGAL_CHAR;
+    }
+
+    for (int i = 0; i < strlen(cypherText); i++) {
+        int indexCypher = getIndex(cypherText[i], CYPHER_CHARACTERS);
+        int indexKey = getIndex(key.chars[i % strlen(key.chars)], KEY_CHARACTERS) + 1;
+
+        output[i] = MESSAGE_CHARACTERS[indexCypher ^ indexKey] - 1;
+    }
+    output[strlen(cypherText)] = '\0';
     return 0;
 }
--- crypter/main.c
+++ crypter/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
     KEY* myKey = malloc(sizeof(myKey));
-    char* cypher = argv[1];
+    char* key = argv[1];
 
     // check number of args
     if (argc < 2) {
@@ -16,7 +16,6 @@ int main(int argc, char* argv[]) {
     } else {
         // check the key
         while(*argv[1] != '\0') {
-            // dirty ASCII
             if(*argv[1] < 'A' || *argv[1] > 'Z') {
                 fprintf(stderr, "Ungültiger Schlüssel\n");
                 free(myKey);
@@ -25,66 +24,50 @@ int main(int argc, char* argv[]) {
             argv[1]++;
         }
 
-        // cypherText
-        printf("Der Schlüssel lautet: %s\n",cypher);
-
         // check for input file
         if(argc == 3) {
             char c;
-            FILE *fp = fopen(argv[2], "r");
+            FILE* fp = fopen(argv[2], "r");
             if(fp != NULL) {
                 while((c = fgetc(fp)) != EOF) {
                     // TODO - fread
+                    fclose(fp);
                 }
             } else {
+                fclose(fp);
                 fprintf(stderr, "Datei nicht lesbar\n");
                 return E_MESSAGE_ILLEGAL_CHAR;
             }
-
         }
 
         // read from stdin
-        char* input = malloc(sizeof(char)*255);
+        char input[255];
         
         if(argc == 2) {    
-            printf("Bitte geben Sie ihr Klartextpasswort bzw. Cypher ein:\n");
-            fgets(input, 255, stdin);
-        }
-
-        // resize cypher if needed
-        char* finalCypher;
-
-        if(strlen(cypher) < strlen(input)) {
-            int diff = strlen(input) - strlen(cypher);
-            char strTemp[diff-1];
-            finalCypher = cypher;
-            strncpy(strTemp, cypher, diff-1);
-            strcat(finalCypher, strTemp);
-        } else {
-            finalCypher = cypher;
+            printf("Bitte geben Sie ihr Klartextpasswort bzw. Cipher ein:\n");
+            scanf("%s", input);
         }
 
         // setup output memory
-        char* output = malloc(sizeof(char)*255);
+        char output[strlen(input)];
 
         // setup the Key
-        myKey->chars = finalCypher;
+        myKey->chars = key;
         myKey->type = 1;
 
         if(strstr(argv[0], "encrypt") != NULL) {
-            encrypt(*myKey, input, output);
+            if(encrypt(*myKey, input, output) == 0) {
+                printf("Das Ergebnis lautet: %s\n", output);
+            }
         }
 
         if(strstr(argv[0], "decrypt") != NULL) {
-            decrypt(*myKey, input, output);
+            if(decrypt(*myKey, input, output) == 0) {
+                printf("Das Ergebnis lautet: %s\n", output);
+            }
         }
-
-        // print result
-        printf("Das Ergebnis lautet: %s\n", output);
-
+        
         free(myKey);
-        free(input);
-        free(output);
         return 0;
     }
 }
